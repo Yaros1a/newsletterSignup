347ca3a449ed9f6b84f5e108d94534bf526e3226
Yarik <yarik.yaroslav2002@gmail.com>
2023-05-17 16:26:36 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,66 @@
+const express  = require("express")
+const bodyParser = require("body-parser")
+const request = require("request")
+const https = require("https")
+
+const app = express()
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Server is listening on port 3000..")
+})
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/signup.html")
+})
+
+app.post("/failure", function(req, res){
+    res.redirect("/");
+})
+
+app.post("/", function(req, res){
+    const firstName = req.body.name;
+    const lastName = req.body.surname;
+    const email = req.body.email;
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName,
+                    
+                }
+            }
+        ]
+    };
+    const jsonData = JSON.stringify(data);
+    const url = "https://us12.api.mailchimp.com/3.0/lists/856c94c561"
+    const options = {
+        method: "POST",
+        auth: "yarik:03b7bc7030bc88b3800bae7f147dcc52-us12"
+    }
+    
+    const request = https.request(url, options, function(response){
+        
+        if(response.statusCode === 200){
+            res.sendFile(__dirname + "/success.html")
+        } else {
+            res.sendFile(__dirname + "/failure.html")
+        }
+        
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        })
+    })
+
+    request.write(jsonData);
+    request.end()
+
+})
+
+//API 03b7bc7030bc88b3800bae7f147dcc52-us12
+//id 856c94c561
